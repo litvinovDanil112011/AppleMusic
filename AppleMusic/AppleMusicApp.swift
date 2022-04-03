3ca0b0ae27780a48a03ef6ae6eac14def4b969be
litvinovDanil112011 <danil112011@mail.ru>
2022-04-03 17:30:11 +0300

    Initial Commit

---
+++ AppleMusic/AppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicApp.swift
+//  AppleMusic
+//
+//  Created by Daniil Litvinov on 03.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
