33fc41f98f22c4c894208a7d871dba699c67b214
litvinovDanil112011 <danil112011@mail.ru>
2022-04-03 18:33:50 +0300

    grene library and screen Mai.

--- AppleMusic/View/ContentView.swift
+++ AppleMusic/View/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            textScreenView
+            .navigationTitle("Медиатека")
+            .navigationBarItems(trailing: NavigationLink("Править", destination: {
+                LibraryView()
+            }))
+        }
+        
     }
 }
 
---
+++ AppleMusic/View/TextScreenView.swift
@@ -0,0 +1,25 @@
+//
+//  TextScreenView.swift
+//  AppleMusic
+//
+//  Created by Daniil Litvinov on 03.04.2022.
+//
+
+import SwiftUI
+
+
+    var textScreenView: some View {
+        VStack{
+            Text("Ищите свою музыку?")
+                .font(.title)
+                .foregroundColor(.black)
+                .padding()
+            Text("Здесь появиться купленная Вами в")
+                .foregroundColor(.gray)
+                .font(.system(size: 15))
+            Text("iTenus Store музыка.")
+                .foregroundColor(.gray)
+                .font(.system(size: 15))
+        }
+    }
+
